1a7da302a30610c3fed41e8e31d1e42754488c90
Sergey Chrul <siberianodis@gmail.com>
2018-05-25 15:17:05 +0200

    Add new test case about Contacts and additional Model file

---
+++ Models/contact.py
@@ -0,0 +1,25 @@
+__author__ = 'Sergey Khrul'
+
+class Contact:
+    def __init__(self, first_name, middle_name, last_name, nickname, title, company, address, home, mobile_phone,
+                 work_phone, fax_phone, email, email2, email3, homepage, birth_date, anniversary_date, address2, phone2, notes):
+        self.first_name = first_name
+        self.middle_name = middle_name
+        self.last_name = last_name
+        self.nickname = nickname
+        self.title = title
+        self.company = company
+        self.address = address
+        self.home = home
+        self.mobile_phone = mobile_phone
+        self.work_phone = work_phone
+        self.fax_phone = fax_phone
+        self.email = email
+        self.email2 = email2
+        self.email3 = email3
+        self.homepage = homepage
+        self.birth_date = birth_date
+        self.anniversary_date = anniversary_date
+        self.address2 = address2
+        self.phone2 = phone2
+        self.notes = notes
--- test_add_group.py
+++ test_add_group.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from selenium.webdriver.firefox.webdriver import WebDriver
 import unittest
-from group import Group
+from Models.group import Group
 
 def is_alert_present(wd):
     try:
